82d07fe045a29e7820295e25eeb4c255571fd39e
SUBHIKSHA <SUBHIKSHA@LAPTOP-75EJ6CAN>
2023-03-24 22:32:51 -0400

    Add project files.

---
+++ SchoolProject/Models/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using Mysqlx.Datatypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolProject.Models
+{
+    public class Teacher
+    {
+        public int TeacherId;
+         public double Salary;
+          public string Employeenumber;
+            public string Hiredate;
+            public string TeacherFname; 
+            public string TeacherLname;
+    }
+}
+
